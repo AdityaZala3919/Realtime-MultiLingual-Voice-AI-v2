2e87252a9ff2f7ba2246fd8c24e9eedacd0b7f14
adityasinh-zala <adityasinhz@webelight.co.in>
2026-03-02 15:58:58 +0530

    v2: updated stt.py

--- stt.py
+++ stt.py
@@ -6,6 +6,7 @@ sarvam_client = AsyncSarvamAI(api_subscription_key=os.getenv("SARVAM_API_KEY"))
 
 async def transcribe_wav(wav_bytes: bytes):
     resp = await sarvam_client.speech_to_text.translate(
-        file=_io.BytesIO(wav_bytes), model="saaras:v3"
+        file=_io.BytesIO(wav_bytes),
+        model="saaras:v3"
     )
     return resp.transcript, resp.language_code
